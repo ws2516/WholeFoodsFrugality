418c302f26cdececec89fb0ccfd262f299cc845b
William Shamma <william.shamma2@gmail.com>
2021-06-04 12:03:08 -0400

    procfile

--- model/get_nearby_names.py
+++ model/get_nearby_names.py
@@ -5,6 +5,7 @@ import webdriver_manager
 import bs4
 import pandas as pd
 from bs4 import BeautifulSoup
+import os
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -17,19 +18,21 @@ from selenium.common.exceptions import TimeoutException
 from webdriver_manager.chrome import ChromeDriverManager
 
 def get_list_of_names(cityname):
-    try:
-        options = Options()
-        options.add_argument('--headless')
-        options.add_argument('--disable-gpu') 
-        driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
-        driver.get("https://www.wholefoodsmarket.com/sales-flyer")
-        inputElement = driver.find_element_by_id("store-finder-search-bar")
-        inputElement.send_keys(str(cityname))
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "wfm-search-bar--list_item")))
-        saved =  driver.page_source
-        driver.close()
-        return saved
-    except:
+    if True:
+    	chrome_options = webdriver.Options()
+    	chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    	chrome_options.add_argument("--headless")
+    	chrome_options.add_argument("--disable-dev-shm-usage")
+    	chrome_options.add_argument("--no-sandbox")
+    	driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    	driver.get("https://www.wholefoodsmarket.com/sales-flyer")
+    	inputElement = driver.find_element_by_id("store-finder-search-bar")
+    	inputElement.send_keys(str(cityname))
+    	WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "wfm-search-bar--list_item")))
+    	saved =  driver.page_source
+    	driver.close()
+    	return saved
+    else:
     	return "Oops, there does not seem to be a Whole Foods near you."
 
 def get_names_nearby(page_sourced):
