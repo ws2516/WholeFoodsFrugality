554561d23431a7ff8e346f43f4ba61c7946651d2
William Shamma <william.shamma2@gmail.com>
2021-06-06 16:07:18 -0400

    adding pipline now again - harder than expected

--- data_pipeline.py
+++ data_pipeline.py
@@ -1,6 +1,8 @@
 import update_with_df
 from update_with_df import write_to_sheet
 import pandas as pd
+import model
+from model import combination_file
 
 
-print(update_with_df.write_to_sheet(pd.DataFrame({'Hello':[1,2,3]}),'Try'))
+print(update_with_df.write_to_sheet(combination_file.go('1234','Whole Foods')))
