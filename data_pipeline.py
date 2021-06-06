0935b7f69e6559f378361dded961640b8e5e4b5e
William Shamma <william.shamma2@gmail.com>
2021-06-06 16:15:35 -0400

    fixing json

--- data_pipeline.py
+++ data_pipeline.py
@@ -2,7 +2,7 @@ import update_with_df
 from update_with_df import write_to_sheet
 import pandas as pd
 import model
-from model import combination_file
+from model import aldis_us_sale
 
 
-print(update_with_df.write_to_sheet(combination_file.go('1234','Whole Foods')))
+print(update_with_df.write_to_sheet(aldis_us_sale.items_on_sale()))
