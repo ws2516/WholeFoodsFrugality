013edb505a1d14c46230c26aa4dbcc042111ccf1
William Shamma <william.shamma2@gmail.com>
2021-06-06 16:41:01 -0400

    adding retreival pipeline

--- model/combination_file.py
+++ model/combination_file.py
@@ -8,11 +8,11 @@ from model import aldis_uk_sale
 def go(inputs, store_name):
 	if store_name == 'WholeFoods':
 		final_df = whole_foods_sale.items_on_sale()
-	elif store_name == 'Aldis AU':
+	elif store_name == 'Aldi AU':
 		final_df = aldis_au_sale.items_on_sale()
-	elif store_name == 'Aldis US':
+	elif store_name == 'Aldi US':
 		final_df = aldis_us_sale.items_on_sale()
-	elif store_name == 'Aldis UK':
+	elif store_name == 'Aldi UK':
 		final_df = aldis_us_sale.items_on_sale()
 	return final_df.to_html()
 	
