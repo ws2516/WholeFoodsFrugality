b9457edc8260c9fb8ac095bf42d6ff9622dc8811
William Shamma <william.shamma2@gmail.com>
2021-06-06 15:52:23 -0400

    adding pipline now

--- data_pipeline.py
+++ data_pipeline.py
@@ -1,5 +1,5 @@
-import gs_data_push
-from gs_data_push import write_to_sheet
+import update_with_df
+from update_with_df import write_to_sheet
 
 
-print(gs_data_push.write_to_sheet(pd.DataFrame({'Hello':[1,2,3]}),'Try'))
+print(update_with_df.write_to_sheet(pd.DataFrame({'Hello':[1,2,3]}),'Try'))
