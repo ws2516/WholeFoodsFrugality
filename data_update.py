de562befd7912cdfc16201e6d527417bdbe15e86
William Shamma <william.shamma2@gmail.com>
2021-06-06 16:52:32 -0400

    adding retreival pipeline

---
+++ data_update.py
@@ -0,0 +1,16 @@
+import update_with_df
+from update_with_df import write_to_sheet
+import pandas as pd
+import model
+from model import aldis_us_sale
+from model import aldis_au_sale
+from model import whole_foods_sale
+
+def update_all():
+	update_with_df.write_to_sheet(whole_foods_sale.items_on_sale(),'Whole Foods')
+	update_with_df.write_to_sheet(aldis_au_sale.items_on_sale(),'Aldi AU')
+	update_with_df.write_to_sheet(aldis_us_sale.items_on_sale(),'Aldi US')
+	update_with_df.write_to_sheet(aldis_us_sale.items_on_sale(),'Aldi UK')
+	return 'Done'
+
+print(update_all())
